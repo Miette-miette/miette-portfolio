3987129c3bc7b3fb6169680160781343799dbe6a
miette <leguyader.doriane@gmail.com>
2025-03-10 17:15:51 +0100

    creation des composants header et section

---
+++ src/app/_components/Header.tsx
@@ -0,0 +1,10 @@
+export const Header = () => {
+    return (
+        <header className="sticky top-0">
+            <h1>Miette</h1>
+            <div>
+
+            </div>
+        </header>
+    )
+}
---
+++ src/app/_components/Section.tsx
@@ -0,0 +1,7 @@
+export const Section = () => {
+    return(
+    <section className="max-w-3xl px-4 m-auto">
+
+    </section>
+    )
+}
