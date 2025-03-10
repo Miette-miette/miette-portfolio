f4d21fc0226fc1ea1ab02d660227b083172c1377
miette <leguyader.doriane@gmail.com>
2025-03-10 18:53:22 +0100

    creation de la structure des pages

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,10 +1,44 @@
-export const Header = () => {
-    return (
-        <header className="sticky top-0">
-            <h1>Miette</h1>
-            <div>
+'use client'
 
+import React , { Component, useState } from 'react';
+
+export default function Header() {
+
+//Menu
+
+const [showMenu, setShowMenu] = useState(false)
+
+const handleShowMenu = () =>{
+  setShowMenu(!showMenu)
+};
+    return (
+        <header className="sticky top-0 ">
+            <div id="header-content" className='flex flex-row justify-between'>
+                <a href="/" id="logo" className='z-3'>
+                    <img src="./media/logo/miettelogosolo.png"  className="size-20"/>
+                </a>
+
+                <div id="navbar-burger" className='z-3' onClick={handleShowMenu}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
+                        <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+                    </svg>
+                </div>
             </div>
+
+            <nav className={`navbar-links ${showMenu ? "show-nav" : "hide"}`}>
+                <ul>
+                    <li>
+                        <a>Design Graphique</a>
+                    </li>
+                    <li>
+                        <a>Projets Web</a>
+                    </li>
+                    <li>
+                        <a>Vidéos et Illustrations</a>
+                    </li>
+                </ul>
+            </nav>
+
         </header>
     )
 }
