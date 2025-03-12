2a8948f76a884e8d24f6e0e0de19036820f123f2
miette <leguyader.doriane@gmail.com>
2025-03-12 17:12:29 +0100

    flip-card et animation Presentation

--- src/app/_components/Presentation.tsx
+++ src/app/_components/Presentation.tsx
@@ -7,51 +7,44 @@ export default function Presentation() {
 useEffect(()=>{
   sr.reveal('.mount1', {
     origin: 'bottom',
-    distance: '500px',
+    distance: '200px',
     scale: 1,
     easing: 'ease',
-    delay: 100,
-    duration: 5000,
-    reset: false
+    delay: 50,
+    duration: 6000,
+    reset: true
     })
 },[])
 useEffect(()=>{
     sr.reveal('.mount2', {
       origin: 'bottom',
-      distance: '500px',
+      distance: '300px',
       scale: 1,
       easing: 'ease',
-      delay: 200,
-      duration: 4000,
-      reset: false
+      delay: 150,
+      duration: 5000,
+      reset: true
       })
   },[])
 
   useEffect(()=>{
     sr.reveal('.mount3', {
       origin: 'bottom',
-      distance: '500px',
+      distance: '400px',
       scale: 1,
       easing: 'ease',
-      delay: 300,
-      duration: 2000,
-      reset: false
+      delay: 100,
+      duration: 3000,
+      reset: true
       })
   },[])
 
 return(
-    <section id="presentation" >
-
-          <div className="wrapper">
-            <h2> Qui suis-je? </h2>
-            <p>Autodidacte passionnée par le développement web, mon souhait est de faire de ce passe-temps mon métier.<br/>
-                Étant titulaire d’un DNSEP (Diplôme National Supérieur d’Expression Plastique) option Design de la transition, le Design Thinking et la créativité sont essentiels à ma méthodologie.<br/>
-                Actuellement en attente de validation de la formation à distance DWWM de l'EPSI, je souhaite me professionnaliser dans ce domaine et ainsi concrétiser mon désir de reconversion.
-            </p>
-            <button>En savoir plus!</button>
-          </div>
-
-          <div id="presentation-anim" className="flex justify-center items-center">
+    <section id="presentation" className='h-800px' >
+          
+          <h2 className='flex justify-center m-10'> Qui suis-je? </h2>
+          
+          <div id="presentation-anim" className=" flex justify-center items-center relative z-3">
             <img src="./media/anim-laptop/corps.png" id="corps" className="size-100 z-4 relative"/>
             <img src="./media/anim-laptop/tete.png" id="tete" className="size-100 z-5 absolute"/>
             <img src="./media/anim-laptop/bras.png" id="bras" className="size-100 z-6 absolute"/>
@@ -59,15 +52,27 @@ return(
 
           </div>
 
-          <div id="bird-anim" className='flex items-start justify-between'>
-            <img src="./media/backgrounds/oiseau1.svg" className="size-40 z-7 absolute"/>
+          <div id="bird-anim" className='flex items-end justify-center'>
+            <img src="./media/backgrounds/oiseau1.svg" id="bird1" className="size-50 z-7 absolute left-70"/>
+            <img src="./media/backgrounds/oiseau2.svg" id="bird2" className="size-50 z-7 absolute right-70 mb-50"/>
           </div>
 
-          <div id="bg-anim" className="mountAnim flex flex-col justify-end" >
+          <div id="bg-anim" className="mountAnim flex flex-col justify-end z-0" >
             <img src="./media/backgrounds/montagne1.svg" className="mount1 z-2 absolute "/>
             <img src="./media/backgrounds/montagne2.svg" className="mount2 z-1 absolute "/>
             <img src="./media/backgrounds/montagne3.svg" className="mount3 z-0 absolute "/>
+
+            <div className="wrapper relative z-3">
+            
+            <p>Autodidacte passionnée par le développement web, mon souhait est de faire de ce passe-temps mon métier.<br/>
+                Étant titulaire d’un DNSEP (Diplôme National Supérieur d’Expression Plastique) option Design de la transition, le Design Thinking et la créativité sont essentiels à ma méthodologie.<br/>
+                Actuellement en attente de validation de la formation à distance DWWM de l'EPSI, je souhaite me professionnaliser dans ce domaine et ainsi concrétiser mon désir de reconversion.
+            </p>
+            <button>En savoir plus!</button>
           </div>
+          </div>
+          
+          
         </section>
 )
 }
--- src/app/_components/Welcome.tsx
+++ src/app/_components/Welcome.tsx
@@ -1,10 +1,10 @@
 'use client'
 
 import React, { useEffect } from 'react';
-import dynamic from "next/dynamic";
+
+
 
 export default function Welcome() {
-
 
   return (
     <section id="welcome" className="flex justify-center items-center">
@@ -13,13 +13,40 @@ export default function Welcome() {
             <img src="./media/logo/logo-miette.png"/>
             <h1>Developpement Web | Design Graphique</h1> 
             <button></button>
-          </div>
+        </div>
 
-          <div id="welcome-visuel" className="flex flex-col z-1 w-120">
-            <img src="./media/doodle-anim/doodle1.svg" id='doodle1' className="size-40 z-4 absolute"/>
-            <img src="./media/doodle-anim/doodle2.svg" id='doodle2' className="size-40 z-4 absolute"/>
-            <img src="./media/doodle-anim/doodle3.svg" id='doodle3' className="size-40 z-4 absolute"/>
-          </div>
-        </section>
+        <div id="welcome-visuel" className="flex flex-col z-1 w-120">
+
+            <div className='flip-doodle absolute top-30 left-50'>
+                <div className='flip-doodle-inner'>
+                    <div className='front-doodle flex justify-center items-center'>
+                        <img src="./media/doodle-anim/doodle3.svg" className="size-60 z-4 absolute"/>
+                    </div>
+                    <div className='back-doodle flex justify-center items-center'>
+                        <img src="./media/doodle-anim/doodle1back.svg" className="size-60 z-4 absolute"/>
+                        <h3 className='z-5 absolute'>Festival<br/> Nation Sounds</h3>
+                    </div>
+                </div>   
+            </div>
+
+            <div className='flip-doodle absolute top-80 right-60'>
+                <div className='flip-doodle-inner'>
+                    <div className='front-doodle flex justify-center items-center'>
+                        <img src="./media/doodle-anim/doodle1.svg" className="size-50 z-4 absolute"/>
+                    </div>
+                    <div className='back-doodle flex justify-center items-center'>
+                        <img src="./media/doodle-anim/doodle1back.svg" className="size-50 z-4 absolute"/>
+                        <button className='z-5 absolute'>
+                            <h3 >De Toi à Moi<br/> et les<br/> P'tits Bouts</h3>
+                        </button>
+                        
+                    </div>
+                </div>   
+            </div>
+            
+        </div>
+
+        
+    </section>
   );
 }
