91d0582a4b8f060debe218dde670b396a2ac7740
miette <leguyader.doriane@gmail.com>
2025-04-15 11:14:29 +0200

    ajout margin domaine

--- src/app/_components/Domaines.jsx
+++ src/app/_components/Domaines.jsx
@@ -15,21 +15,21 @@ export default function Domaine() {
             <div className='flex flex-col md:flex-row text-[#B36E7B] p-5 md:pr-60 md:pl-60'>
 
                 <div className='bento-beige flex flex-col'>
-                    <Image src="/media/domaines/dev.png" height={80} alt='illu dev'/>
+                    <Image src="/media/domaines/dev.png" className='m-2' height={80} alt='illu dev'/>
                     <h3 className='text-2xl'>Développement Web</h3>
                     <p>Conception de site</p>
                     <p>Front-End et Back-End</p>
                 </div>
 
                 <div className='bento-beige flex flex-col'>
-                    <Image src="/media/domaines/design.png" height={80} alt='illu design'/>
+                    <Image src="/media/domaines/design.png" className='m-2' height={80} alt='illu design'/>
                     <h3 className='text-2xl'>Design Graphique</h3>
                     <p>Réalisation de charte graphique et logo</p>
                     <p>Support web et print</p>
                 </div>
 
                 <div className='bento-beige flex flex-col'>
-                    <Image src="/media/domaines/illu.png" height={80} alt='illu design'/>
+                    <Image src="/media/domaines/illu.png" className='m-2' height={80} alt='illu design'/>
                     <h3 className='text-2xl'>Illustration</h3>
                     <p>Illustrations tradi & digitales</p>
                     <p>Animation stop-motion/ papier découpés</p>
